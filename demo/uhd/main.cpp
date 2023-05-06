df9893d3c66847ad2cc3610e86c80a27222cbcf2
jcc <995586238@qq.com>
2023-05-06 10:24:20 +0800

    add demo c/c++ api

---
+++ demo/uhd/main.cpp
@@ -0,0 +1,66 @@
+#include <uhd/utils/thread.hpp>
+#include <uhd/utils/safe_main.hpp>
+#include <uhd/usrp/multi_usrp.hpp>
+#include <uhd/exception.hpp>
+#include <uhd/types/tune_request.hpp>
+#include <boost/program_options.hpp>
+#include <boost/format.hpp>
+#include <boost/thread.hpp>
+#include <iostream>
+
+int UHD_SAFE_MAIN(int argc, char *argv[]) {
+    uhd::set_thread_priority_safe();
+
+    std::string device_args("type=ant");
+    std::string ant("TX/RX");
+    std::string ref("internal");
+
+    double rate(1e6);
+    double freq(915e6);
+    double gain(10);
+    double bw(1e6);
+
+    //create a usrp device
+    std::cout << std::endl;
+    std::cout << boost::format("Creating the usrp device with: %s...") % device_args << std::endl;
+    uhd::usrp::multi_usrp::sptr usrp = uhd::usrp::multi_usrp::make(device_args);
+
+    // Lock mboard clocks
+    std::cout << boost::format("Lock mboard clocks: %f") % ref << std::endl;
+    usrp->set_clock_source(ref);
+    
+
+    //set the sample rate
+    if (rate <= 0.0) {
+        std::cerr << "Please specify a valid sample rate" << std::endl;
+        return ~0;
+    }
+
+    // set sample rate
+    std::cout << boost::format("Setting RX Rate: %f Msps...") % (rate / 1e6) << std::endl;
+    usrp->set_rx_rate(rate);
+    std::cout << boost::format("Actual RX Rate: %f Msps...") % (usrp->get_rx_rate() / 1e6) << std::endl << std::endl;
+
+    // set freq
+    std::cout << boost::format("Setting RX Freq: %f MHz...") % (freq / 1e6) << std::endl;
+    uhd::tune_request_t tune_request(freq);
+    usrp->set_rx_freq(tune_request);
+    std::cout << boost::format("Actual RX Freq: %f MHz...") % (usrp->get_rx_freq() / 1e6) << std::endl << std::endl;
+
+    // set the rf gain
+    std::cout << boost::format("Setting RX Gain: %f dB...") % gain << std::endl;
+    usrp->set_rx_gain(gain);
+    std::cout << boost::format("Actual RX Gain: %f dB...") % usrp->get_rx_gain() << std::endl << std::endl;
+
+    // set the IF filter bandwidth
+    std::cout << boost::format("Setting RX Bandwidth: %f MHz...") % (bw / 1e6) << std::endl;
+    usrp->set_rx_bandwidth(bw);
+    std::cout << boost::format("Actual RX Bandwidth: %f MHz...") % (usrp->get_rx_bandwidth() / 1e6) << std::endl << std::endl;
+
+    // set the antenna
+    std::cout << boost::format("Setting RX Antenna: %s") % ant << std::endl;
+    usrp->set_rx_antenna(ant);
+    std::cout << boost::format("Actual RX Antenna: %s") % usrp->get_rx_antenna() << std::endl << std::endl;
+
+    return EXIT_SUCCESS;
+}
